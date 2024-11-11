f3a0f745ca5c66eb114894f8685cb86b635bca7e
Jeremy Schur <jeremy.schur02@gmail.com>
2024-11-10 21:49:11 -0700

    changed docker compose for dev and prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const { VITE_API_URL, VITE_WEB_PORT } = env;
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server : {
       host: true,
-      port: VITE_WEB_PORT,
-      strictPort: true,
-      // proxy: {
-      //   '/api': {
-      //     target: VITE_API_URL,
-      //     changeOrigin: true,
-      //     rewrite: (path) => path.replace(/^\/api/, '')
-      //   }
-      // },
       watch: {
         usePolling: true,
       }
