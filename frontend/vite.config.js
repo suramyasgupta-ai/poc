58acb50174f8283c0587c66691c9e28c23d2c33c
Jeremy Schur <jeremy.schur02@gmail.com>
2024-11-13 22:53:27 -0700

    setup dev and prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
     plugins: [react()],
     server : {
       host: true,
       watch: {
-        usePolling: true,
+        usePolling: !isProduction,
       }
+    },
+    define: {
+      'process.env.MAPBOX_ACCESS_TOKEN': JSON.stringify(env.MAPBOX_ACCESS_TOKEN),
+      'process.env.API_URL': JSON.stringify(env.API_URL),
     }
   };
 });
