e067b9dad54c21b8742a39ad5026b7323c136b73
Jeremy Schur <jeremy.schur02@gmail.com>
2024-11-29 21:54:20 -0700

    adding origin header

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 const BASE_URL = process.env.API_URL;
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    headers: {
+        'Origin': BASE_URL
+    }
 });
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
+    headers: {
+        'Origin': BASE_URL
+    }
 });
