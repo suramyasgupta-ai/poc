beb1b86b96d46a921c05d83ef4951e50cb635acd
Jeremy Schur <jeremy.schur02@gmail.com>
2024-11-29 17:17:07 -0700

    update to nginx

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 const BASE_URL = process.env.API_URL;
 
-export default axios.create({});
+export default axios.create({
+    baseURL: BASE_URL
+});
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
