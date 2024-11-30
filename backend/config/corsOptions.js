11217388454c2bf4cbbc21c8881be80e50f84b04
Jeremy Schur <jeremy.schur02@gmail.com>
2024-11-29 20:37:26 -0700

    update to nginx.conf

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -8,18 +8,7 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true,
     optionsSuccessStatus: 200,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
-    allowedHeaders: [
-        'Content-Type',
-        'Authorization',
-        'X-Requested-With',
-        'Accept',
-        'Origin',
-        'Cache-Control',
-        'X-CSRF-Token'
-    ]
 };
 
 module.exports = corsOptions;
