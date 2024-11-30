e952e2b5a4f2983eb0b541005baeca5fd77c559e
Jeremy Schur <jeremy.schur02@gmail.com>
2024-11-29 21:59:40 -0700

    change for origin header

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,15 +3,9 @@ const BASE_URL = process.env.API_URL;
 
 export default axios.create({
     baseURL: BASE_URL,
-    headers: {
-        'Origin': BASE_URL
-    }
 });
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
-    headers: {
-        'Origin': BASE_URL
-    }
 });
